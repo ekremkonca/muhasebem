5810cc2720a91485b1bd081e7af529f480ed83e7
ekremkonca <123271604+ekremkonca@users.noreply.github.com>
2026-08-26 21:40:18 +0300

    Route app by clean page paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import AssetsNav from './AssetsNav.jsx';
-import FontSwitcher from './FontSwitcher.jsx';
-import AssetsHeaderBridge from './AssetsHeaderBridge.jsx';
+import SiteRouter from './SiteRouter.jsx';
 import './styles.css';
 import './header-left.css';
 import './assets-page.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
-      <App />
-      <AssetsNav />
-      <FontSwitcher />
-      <AssetsHeaderBridge />
-    </>
+    <SiteRouter />
   </React.StrictMode>
 );
 
