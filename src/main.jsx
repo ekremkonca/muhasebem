945048ae0f440c3603dadf17f98211bdae1465f2
ekremkonca <123271604+ekremkonca@users.noreply.github.com>
2026-08-26 16:34:07 +0300

    Load Varlıklar/Fon navigation

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 import './header-left.css';
+import './assets-page.css';
+import './assets-page.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
