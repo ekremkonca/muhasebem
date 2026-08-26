aee31fcde5d4686f7dac9f53e6c068ce441511a2
ekremkonca <123271604+ekremkonca@users.noreply.github.com>
2026-08-26 18:45:36 +0300

    Mount assets header bridge

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import AssetsNav from './AssetsNav.jsx';
 import FontSwitcher from './FontSwitcher.jsx';
+import AssetsHeaderBridge from './AssetsHeaderBridge.jsx';
 import './styles.css';
 import './header-left.css';
 import './assets-page.css';
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
       <App />
       <AssetsNav />
       <FontSwitcher />
+      <AssetsHeaderBridge />
     </>
   </React.StrictMode>
 );
