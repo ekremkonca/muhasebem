ec1d2ad6595990486db3e57c560dfb95727e1ecc
ekremkonca <123271604+ekremkonca@users.noreply.github.com>
2026-08-26 23:12:54 +0300

    Bust cache for clean Takvim rebuild

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='rehberlik-muhasebe-v10-directory-pages';
+const CACHE='rehberlik-muhasebe-v12-takvim-known-good';
 const CORE=['/anasayfa/','/muhasebe/','/varliklar/','/takvim/','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
