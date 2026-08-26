363784b40f03c909cd62c9156c47451ec6c5118f
ekremkonca <123271604+ekremkonca@users.noreply.github.com>
2026-08-26 16:37:48 +0300

    Mount Varlıklar/Fon navigation in React

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import AssetsNav from './AssetsNav.jsx';
 import './styles.css';
 import './header-left.css';
 import './assets-page.css';
-import './assets-page.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <>
+      <App />
+      <AssetsNav />
+    </>
   </React.StrictMode>
 );
 
