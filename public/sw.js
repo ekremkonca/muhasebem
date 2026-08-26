f75d2256b7da41dc4877c4aa5b86ba47ec997ee6
ekremkonca <123271604+ekremkonca@users.noreply.github.com>
2026-08-26 17:27:22 +0300

    Refresh PWA cache for final font selector

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='rehberlik-muhasebe-v7-8';
+const CACHE='rehberlik-muhasebe-v7-9';
 const CORE=['/','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
